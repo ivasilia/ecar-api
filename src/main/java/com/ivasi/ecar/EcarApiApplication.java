97be5b9131e3a8098a903ade58a9ebd83f834ef3
ivasilia <ilisavsky@yahoo.com>
2021-03-06 23:45:23 +0200

    init configs

---
+++ src/main/java/com/ivasi/ecar/EcarApiApplication.java
@@ -0,0 +1,13 @@
+package com.ivasi.ecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcarApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivasi/ecar/EcarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivasi.ecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcarApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
